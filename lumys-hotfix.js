07ded773dec37a8495c283465f9a25ce414e9eb7
mythosnetworkbr-cell <mythosnetworkbr@gmail.com>
2026-09-21 04:39:28 -0300

    fix: corrigir definitivamente o filtro de categorias LÜMYS

--- lumys-hotfix.js
+++ lumys-hotfix.js
@@ -2,10 +2,8 @@ const fs = require('node:fs');
 const path = 'index.html';
 if (fs.existsSync(path)) {
   let html = fs.readFileSync(path, 'utf8');
-  html = html.replace(
-    'onclick="filterGenre(\'\'+g+\'\',this)"',
-    'onclick="filterGenre(\\\'\'+g+\'\\\',this)"'
-  );
+  const fixed = "function renderChips(){const genres=['Todos','Romance','Fantasia','Suspense','Ficção científica','Não ficção','Biografia','Infantojuvenil'];$('chips').innerHTML=genres.map((g,i)=>'<button class=\"chip '+(i===0?'active':'')+'\" data-genre=\"'+g+'\">'+g+'</button>').join('');document.querySelectorAll('.chip').forEach(btn=>btn.addEventListener('click',()=>filterGenre(btn.dataset.genre,btn)))}\n";
+  html = html.replace(/function renderChips\(\)\{[\s\S]*?\nfunction filterGenre/, fixed + 'function filterGenre');
   fs.writeFileSync(path, html, 'utf8');
   console.log('LÜMYS hotfix: filtro de categorias corrigido.');
 }
